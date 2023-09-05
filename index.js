c78fcf1bbcda94c2145d876c5fa3af8990886671
AliceLs025 <kokonut280741@gmail.com>
2023-09-06 00:25:56 +0700

    first-commit

---
+++ index.js
@@ -0,0 +1,6 @@
+fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        document.getElementById("activity-name").textContent = data.activity
+    })
