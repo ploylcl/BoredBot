e61e9766e3f31cd4f117f387f845867a88d83548
AliceLs025 <kokonut280741@gmail.com>
2023-09-06 00:42:12 +0700

    css add

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 fetch("https://apis.scrimba.com/bored/api/activity")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        document.getElementById("activity-name").textContent = data.activity
-    })
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+    document.getElementById("activity-name").textContent = data.activity
+})
+
