f32cecee2940fa31b622dac02912870642756870
AliceLs025 <kokonut280741@gmail.com>
2023-09-06 00:47:47 +0700

    add button-event

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-fetch("https://apis.scrimba.com/bored/api/activity")
-.then(response => response.json())
-.then(data => {
-    console.log(data)
-    document.getElementById("activity-name").textContent = data.activity
-})
 
+document.getElementById("activity-click").addEventListener("click" ,function(){
+    fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        document.getElementById("activity-name").textContent = data.activity
+    })
+})
